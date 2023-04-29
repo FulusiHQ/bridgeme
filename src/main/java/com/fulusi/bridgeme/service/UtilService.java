1ef76487faa641452711a4d855ed532eecca8ed9
Linus Ndege <49119538+linus-jijibiz@users.noreply.github.com>
2023-04-28 18:28:49 -0600

    Force release

--- src/main/java/com/fulusi/bridgeme/service/UtilService.java
+++ src/main/java/com/fulusi/bridgeme/service/UtilService.java
@@ -33,7 +33,6 @@ public class UtilService {
         } catch (Exception ex) {
             return null;
         }
-
        // Type userListType = new TypeReference<List<User>>(){}.getType();
        // List<User> users = fromJson(json, userListType);
     }
